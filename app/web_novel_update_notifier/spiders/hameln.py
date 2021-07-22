b86d004098dd22a58e191f5030bb42eaf621b3ba
soltic315 <81360623+soltic315@users.noreply.github.com>
2021-07-23 02:41:36 +0900

    ハーメルンのUI変更対応

--- app/web_novel_update_notifier/spiders/hameln.py
+++ app/web_novel_update_notifier/spiders/hameln.py
@@ -37,17 +37,18 @@ class HamelnSpider(scrapy.Spider):
             yield scrapy.Request(self.start_urls[0], callback=self.parse)
 
         elif page_title == 'お気に入り - ハーメルン':
-            for table in response.xpath('//*[@id="main"]/div[2]/form/div'):
-                title = table.xpath('h3/a[1]/text()').get().strip()
-                author_link = table.xpath('h3/a[2]/text()')
+            for div in response.xpath('//*[@id="main"]/div[2]/form/div').css('.section3'):
+
+                title = div.xpath('h3/a[1]/text()').get().strip()
+                author_link = div.xpath('h3/a[2]/text()')
                 if author_link:
                     author = author_link.get().strip()
                 else:
-                    author = table.xpath('h3/text()').getall()[1].strip()[4:-1]
-                novel_id = table.xpath('h3/a[1]/@href').get().strip().split('/')[-2]
-                temp = table.xpath('p[2]/text()').get().strip()
+                    author = div.xpath('h3/text()').getall()[1].strip()[4:-1]
+                novel_id = div.xpath('h3/a[1]/@href').get().strip().split('/')[-2]
+                temp = div.xpath('p[2]/text()').get().strip()
                 updated_at = datetime.datetime.strptime(temp[:11] + temp[15:], '%Y年%m月%d日%H:%M')
-                latest_url = table.xpath('p[2]/a[1]/@href').get().strip()
+                latest_url = div.xpath('p[2]/a[1]/@href').get().strip()
 
                 yield {
                     'domain': self.allowed_domains[0],
